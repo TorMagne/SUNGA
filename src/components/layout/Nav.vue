dea0ddb5bbc53f3640a6921376addadf9b4ddbce
Tor Magne <tormagne-brekken@outlook.com>
2022-06-15 23:03:20 +0200

    changed the navbar

--- src/components/layout/Nav.vue
+++ src/components/layout/Nav.vue
@@ -1,70 +1,16 @@
 <template>
-  <div v-if="$vuetify.breakpoint.xsOnly">
-    <v-app-bar elevation="0" color="white" height="75" app role="navigation">
-      <router-link to="/"
-        ><v-toolbar-title style="color: #7e57c2"
-          >SUNGA</v-toolbar-title
-        ></router-link
-      >
-      <v-app-bar-nav-icon
-        @click="drawer = true"
-        inline
-        class="ml-auto"
-        style="color: #7e57c2"
-      ></v-app-bar-nav-icon>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary right app>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <div class="d-flex justify-end">
-            <v-btn color="" icon @click="drawer = false">
-              <v-icon style="color: #7e57c2">mdi-close</v-icon>
-            </v-btn>
-          </div>
-          <v-list-item>
-            <router-link to="/infoside" active-class="black--text" class="mr-5"
-              >Home</router-link
-            >
-          </v-list-item>
-          <v-list-item>
-            <router-link
-              to="/brukerside"
-              active-class="black--text"
-              class="mr-5"
-              >Bruker side</router-link
-            >
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </div>
-  <div v-else>
-    <v-app-bar elevation="0" color="white" height="75" app role="navigation">
-      <router-link to="/"
-        ><v-toolbar-title style="color: #7e57c2"
-          >SUNGA</v-toolbar-title
-        ></router-link
-      >
-      <v-list class="ml-auto">
-        <v-list-item-group color="#ffffff">
-          <router-link to="/infoside" active-class="black--text" class="mr-5"
-            >Home</router-link
-          >
-          <router-link to="/brukerside" active-class="black--text" class="mr-5"
-            >Bruker side</router-link
-          >
-        </v-list-item-group>
-      </v-list>
-    </v-app-bar>
-  </div>
+  <NavBarMobile v-if="$vuetify.breakpoint.xsOnly" />
+  <NavBarMdAndUp v-else />
 </template>
 
 <script>
+import NavBarMobile from "./NavBarMobile.vue";
+import NavBarMdAndUp from "./NavBarMdAndUp.vue";
 export default {
+  components: {
+    NavBarMobile,
+    NavBarMdAndUp,
+  },
   data() {
     return {
       drawer: false,
---
+++ src/components/layout/NavBarMdAndUp.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar elevation="0" color="white" height="75" app role="navigation">
+    <router-link to="/"
+      ><v-toolbar-title style="color: #7e57c2"
+        >SUNGA</v-toolbar-title
+      ></router-link
+    >
+    <v-list class="ml-auto">
+      <v-list-item-group color="#ffffff">
+        <router-link to="/infoside" active-class="black--text" class="mr-5"
+          >Home</router-link
+        >
+        <router-link to="/brukerside" active-class="black--text" class="mr-5"
+          >Bruker side</router-link
+        >
+      </v-list-item-group>
+    </v-list>
+  </v-app-bar>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped>
+.v-application a {
+  color: #747474;
+  text-decoration: none;
+  font-size: 18px;
+}
+.v-toolbar__title {
+  font-weight: bold;
+  font-size: 40px;
+}
+</style>
