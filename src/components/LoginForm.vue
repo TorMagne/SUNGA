ef8c8e7be80befc80ada1ca5defef6ba3177d124
Tor Magne <tormagne-brekken@outlook.com>
2022-08-01 13:39:36 +0200

    added vuex 3 and added store file

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -41,6 +41,7 @@
 <script>
 // Import component
 import Loading from "vue-loading-overlay";
+import routerCheck from "../router/index";
 // Import stylesheet
 import "vue-loading-overlay/dist/vue-loading.css";
 import axios from "axios";
